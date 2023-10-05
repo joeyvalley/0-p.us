9bcfb26754a39e98d7b7c5870099719c91ecf649
Joseph Valle <josephrvalle@gmail.com>
2023-10-05 13:15:20 -0400

    removed the content and added the video as the background

---
+++ src/components/Video.jsx
@@ -0,0 +1,7 @@
+export default function Content({ children }) {
+  return (
+    <div className="video-container">
+      {children}
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <>
       <Sidebar></Sidebar>
+      <Video>
+        <video src="/assets/video/3.mp4" autoPlay={true} controls={false} muted={true} className='fullscreen'></video>
+      </Video>
     </>
   )
 }
